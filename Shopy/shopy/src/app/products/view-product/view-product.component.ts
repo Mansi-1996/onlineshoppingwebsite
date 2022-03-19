a45f64db69156dcd01e8a6825b840dd01a7c87e1
Krunal Kamble <krunal.kamble@mastek.com>
2022-03-19 12:02:56 +0530

    activate route

--- Shopy/shopy/src/app/products/view-product/view-product.component.ts
+++ Shopy/shopy/src/app/products/view-product/view-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-product',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProductComponent implements OnInit {
 
-  constructor() { }
+  productID = 0;
+  constructor(private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+     this.activateRoute.params.subscribe(data=>{
+       this.productID= data['id'];
+     })
   }
 
 }
