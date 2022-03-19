821707ab15bc4e1afa7689924bff94792e4c8ed7
Krunal Kamble <krunal.kamble@mastek.com>
2022-03-19 20:05:17 +0530

    In product.service.ts we oberserve the data in mehod what type of data is...,So we add every method with obverveable<Products>. Which shows the data according to proudct.ts .

--- Shopy/shopy/src/app/products/products.service.ts
+++ Shopy/shopy/src/app/products/products.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import{Products} from './products';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +10,22 @@ export class ProductsService {
 
   constructor(private httpClient:HttpClient) { }
 
-  createProduct(productBody: any) {
+  createProduct(productBody: any):Observable<Products> {
     const baseUrl="http://localhost:3000/products";
-    return this.httpClient.post(baseUrl,productBody);
+    return this.httpClient.post<Products>(baseUrl,productBody);
   }
   
-  viewProduct(productId: string){
+  viewProduct(productId: string):Observable<Products>{
     const baseUrl="http://localhost:3000/products"+productId;
-    return this.httpClient.get(baseUrl);
+    return this.httpClient.get<Products>(baseUrl);
   }
 
-  searchCategoryProduct(categoryId: string){
+  searchCategoryProduct(categoryId: string):Observable<Products>{
     const baseUrl="http://localhost:3000/products/categoer="+categoryId;
-    return this.httpClient.get(baseUrl);
+    return this.httpClient.get<Products>(baseUrl);
   }
-  searchDateProduct(dateParam: string){
+  searchDateProduct(dateParam: string):Observable<Products>{
     const baseUrl="http://localhost:3000/products/date="+dateParam;
-    return this.httpClient.get(baseUrl);
+    return this.httpClient.get<Products>(baseUrl);
   }
 }
