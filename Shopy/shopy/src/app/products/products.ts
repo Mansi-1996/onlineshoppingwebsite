ff0315f0f011561f3ed2e5fc0d4d96b8c89e0b12
Krunal Kamble <krunal.kamble@mastek.com>
2022-03-19 19:55:12 +0530

    generated the interface

--- Shopy/shopy/src/app/products/products.ts
+++ Shopy/shopy/src/app/products/products.ts
@@ -1,2 +1,12 @@
 export interface Products {
+    productId:string;
+    categoryId:number;
+    productName:string;
+    description:string;
+    rating:string;
+    price:number;
+    productImg:string;
+    isAvailable:boolean;
+    color:string;
+    review:number;
 }
